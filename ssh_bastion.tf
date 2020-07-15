fceb32909d91edaf2978e67c8c14341b9b126402
Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>
2020-07-15 14:21:02 +0100

    Moving bastion to be in an ASG and related resources
    Signed-off-by: Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>

--- ssh_bastion.tf
+++ ssh_bastion.tf
@@ -87,14 +87,71 @@ resource "aws_iam_instance_profile" "ssh_bastion" {
   role  = aws_iam_role.ssh_bastion[0].name
 }
 
-resource "aws_instance" "ssh_bastion" {
-  count                  = local.ssh_bastion_enabled[local.environment] ? length(data.aws_availability_zones.available.names) : 0
-  ami                    = var.ssh_bastion_ami_id
-  instance_type          = "t2.medium"
-  vpc_security_group_ids = [aws_security_group.ssh_bastion.0.id]
-  subnet_id              = element(aws_subnet.ssh_bastion.*.id, count.index)
-  iam_instance_profile   = aws_iam_instance_profile.ssh_bastion[0].name
-  user_data              = templatefile("${path.module}/ssh_bastion_users.cloud-cfg.tmpl", { users = local.ssh_bastion_users })
+resource "aws_launch_configuration" "ssh_bastion" {
+  count                = local.ssh_bastion_enabled[local.environment] ? 1 : 0
+  name_prefix          = "ssh-bastion-"
+  image_id             = var.ssh_bastion_ami_id
+  instance_type        = "t2.medium"
+  security_groups      = [aws_security_group.ssh_bastion[0].id]
+  iam_instance_profile = aws_iam_instance_profile.ssh_bastion[0].arn
+  user_data            = templatefile("${path.module}/ssh_bastion_users.cloud-cfg.tmpl", { users = local.ssh_bastion_users })
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 50
+    delete_on_termination = true
+    encrypted             = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "ssh_bastion" {
+  count                     = local.ssh_bastion_enabled[local.environment] ? 1 : 0
+  name                      = aws_launch_configuration.ssh_bastion[0].name
+  min_size                  = 3
+  desired_capacity          = 3
+  max_size                  = 3
+  health_check_grace_period = 180
+  health_check_type         = "EC2"
+  force_delete              = true
+  launch_configuration      = aws_launch_configuration.ssh_bastion[0].name
+  vpc_zone_identifier       = aws_subnet.ssh_bastion.*.id
+
+  dynamic "tag" {
+    for_each = merge(
+      local.common_tags,
+      { Name         = "ssh-bastion",
+        Persistence  = "Ignore",
+        AutoShutdown = "False",
+        SSMEnabled   = local.ssh_bastion_ssmenabled[local.environment]
+      },
+    )
+
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb" "ssh_bastion" {
+  count              = local.ssh_bastion_enabled[local.environment] ? 1 : 0
+  name               = "ssh-bastion"
+  internal           = false
+  load_balancer_type = "network"
+  security_groups    = [aws_security_group.ssh_bastion[0].id]
+  depends_on         = [aws_internet_gateway.igw]
+  subnets = [aws_subnet.ssh_bastion[0].id,
+    aws_subnet.ssh_bastion[1].id,
+  aws_subnet.ssh_bastion[2].id]
 
   tags = merge(
     local.common_tags,
@@ -102,14 +159,52 @@ resource "aws_instance" "ssh_bastion" {
   )
 }
 
-resource "aws_eip" "ssh_bastion" {
-  count    = length(aws_instance.ssh_bastion.*.id)
-  vpc      = true
-  instance = aws_instance.ssh_bastion[count.index].id
+resource "aws_lb_target_group" "ssh_bastion" {
+  count       = local.ssh_bastion_enabled[local.environment] ? 1 : 0
+  name        = "ssh-bastion"
+  port        = 22
+  protocol    = "TCP"
+  target_type = "instance"
+  vpc_id      = module.vpc.vpc.id
+
+  stickiness {
+    type    = "lb_cookie"
+    enabled = false
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tags = merge(
     local.common_tags,
-    { Name = "ssh-bastion" }
+    { Name = "ssh-bastion" },
   )
-
 }
+
+resource "aws_lb_listener" "ssh" {
+  count             = local.ssh_bastion_enabled[local.environment] ? 1 : 0
+  load_balancer_arn = aws_lb.ssh_bastion[0].arn
+  port              = 22
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ssh_bastion[0].arn
+  }
+}
+
+
+//resource "aws_eip" "ssh_bastion" {
+//  count    = length(aws_instance.ssh_bastion.*.id)
+//  vpc      = true
+//  instance = aws_instance.ssh_bastion[count.index].id
+//
+//  tags = merge(
+//    local.common_tags,
+//    { Name = "ssh-bastion" }
+//  )
+//
+//  depends_on = [aws_internet_gateway.igw.id]
+//
+//}
