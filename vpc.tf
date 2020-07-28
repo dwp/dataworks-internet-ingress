e63640748bb1bfef95bff1024c9d8dfb0e7ed44a
Benjamin Sherwood <benjamin.sherwood@dmwgroup.co.uk>
2020-07-28 14:29:45 +0100

    DW-4490 - Deploying proxy as-is so that we can create records in mgmt R53 via CI (#34)

--- vpc.tf
+++ vpc.tf
@@ -6,17 +6,17 @@ module "vpc" {
   vpc_cidr_block                           = local.cidr_block[local.environment]["internet-ingress-vpc"]
   interface_vpce_source_security_group_ids = concat(local.reverse_proxy_enabled[local.environment] ? [aws_security_group.reverse_proxy_instance[0].id] : [], local.ssh_bastion_enabled[local.environment] ? [aws_security_group.ssh_bastion[0].id] : [])
   interface_vpce_subnet_ids                = aws_subnet.vpc_endpoint.*.id
-  gateway_vpce_route_table_ids             = aws_route_table.reverse_proxy.*.id
-  aws_vpce_services                        = ["s3", "logs"]
-}
-
-resource "aws_route_table" "reverse_proxy" {
-  vpc_id = module.vpc.vpc.id
-
-  tags = merge(
-    local.common_tags,
-    { Name = "reverse_proxy" },
-  )
+  gateway_vpce_route_table_ids             = aws_route_table.reverse_proxy_private.*.id
+  aws_vpce_services = [
+    "logs",
+    "s3",
+    "ec2",
+    "ec2messages",
+    "kms",
+    "monitoring",
+    "ssm",
+    "ssmmessages"
+  ]
 }
 
 resource "aws_subnet" "vpc_endpoint" {
@@ -31,21 +31,8 @@ resource "aws_subnet" "vpc_endpoint" {
   )
 }
 
-resource "aws_subnet" "reverse_proxy" {
-  count             = length(data.aws_availability_zones.available.names)
-  // start after the vpc_endpoint subnets
-  cidr_block        = cidrsubnet(module.vpc.vpc.cidr_block, 4, count.index + 3)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  vpc_id            = module.vpc.vpc.id
-
-  tags = merge(
-    local.common_tags,
-    { Name = "reverse-proxy" }
-  )
-}
-
 resource "aws_subnet" "ssh_bastion" {
-  count             = length(data.aws_availability_zones.available.names)
+  count = length(data.aws_availability_zones.available.names)
   // start after the reverse_proxy subnets
   cidr_block        = cidrsubnet(module.vpc.vpc.cidr_block, 4, count.index + 3 + 3)
   availability_zone = data.aws_availability_zones.available.names[count.index]
@@ -57,14 +44,94 @@ resource "aws_subnet" "ssh_bastion" {
   )
 }
 
-resource "aws_route_table_association" "reverse_proxy" {
+resource "aws_subnet" "reverse_proxy_public" {
+  count = length(data.aws_availability_zones.available.names)
+  // start after the vpc_endpoint subnets
+  cidr_block        = cidrsubnet(module.vpc.vpc.cidr_block, 4, count.index + 3)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  vpc_id            = module.vpc.vpc.id
+
+  tags = merge(
+    local.common_tags,
+    { Name = "reverse-proxy-public" }
+  )
+}
+
+resource "aws_route_table" "reverse_proxy_public" {
+  count  = length(data.aws_availability_zones.available.names)
+  vpc_id = module.vpc.vpc.id
+
+  tags = merge(
+    local.common_tags,
+    { Name = "reverse_proxy_public" },
+  )
+}
+
+resource "aws_route_table_association" "reverse_proxy_public" {
   count          = length(data.aws_availability_zones.available.names)
-  subnet_id      = aws_subnet.reverse_proxy.*.id[count.index]
-  route_table_id = aws_route_table.reverse_proxy.id
+  subnet_id      = aws_subnet.reverse_proxy_public[count.index].id
+  route_table_id = aws_route_table.reverse_proxy_public[count.index].id
+}
+
+resource "aws_route" "reverse_proxy_public_default_route" {
+  count                  = length(data.aws_availability_zones.available.names)
+  route_table_id         = aws_route_table.reverse_proxy_public[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_subnet" "reverse_proxy_private" {
+  count = length(data.aws_availability_zones.available.names)
+  // start after the vpc_endpoint subnets
+  cidr_block        = cidrsubnet(module.vpc.vpc.cidr_block, 4, count.index + 3 + 3 + 3)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  vpc_id            = module.vpc.vpc.id
+
+  tags = merge(
+    local.common_tags,
+    { Name = "reverse-proxy-private" }
+  )
+}
+
+resource "aws_route_table" "reverse_proxy_private" {
+  count  = length(data.aws_availability_zones.available.names)
+  vpc_id = module.vpc.vpc.id
+
+  tags = merge(
+    local.common_tags,
+    { Name = "reverse_proxy_private" },
+  )
+}
+
+resource "aws_route_table_association" "reverse_proxy_private" {
+  count          = length(data.aws_availability_zones.available.names)
+  subnet_id      = aws_subnet.reverse_proxy_private[count.index].id
+  route_table_id = aws_route_table.reverse_proxy_private[count.index].id
+}
+
+resource "aws_route" "reverse_proxy_private_default_route" {
+  count                  = length(data.aws_availability_zones.available.names)
+  route_table_id         = aws_route_table.reverse_proxy_private[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw.id
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = module.vpc.vpc.id
+
+  tags = merge(
+    local.common_tags,
+    { Name = "internet-ingress" },
+  )
+}
+
+resource "aws_eip" "nat_gw" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_gw.id
+  subnet_id     = aws_subnet.reverse_proxy_public[0].id
 
   tags = merge(
     local.common_tags,
