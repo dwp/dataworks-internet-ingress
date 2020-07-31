beb969f896f3adbe397eaad5617763ca43a3e105
Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>
2020-07-31 11:58:27 +0100

    Fixing lookups and names
    Signed-off-by: Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>

--- reverse_proxy.tf
+++ reverse_proxy.tf
@@ -491,9 +491,9 @@ data "aws_iam_policy_document" "reverse_proxy_assume_role" {
   }
 }
 
-resource "aws_vpc_peering_connection" "reverse_proxy_dev" {
+resource "aws_vpc_peering_connection" "reverse_proxy" {
   count         = local.reverse_proxy_enabled[local.environment] ? 1 : 0
-  peer_owner_id = local.account["development"]
+  peer_owner_id = local.account[local.mgmt_account_mapping[local.environment]]
   peer_vpc_id   = data.terraform_remote_state.ingest.outputs.ingestion_vpc.id
   vpc_id        = module.vpc.vpc.id
 
@@ -504,23 +504,23 @@ resource "aws_vpc_peering_connection" "reverse_proxy_dev" {
 }
 
 resource "aws_route" "reverse_proxy_to_ingest" {
-  count                     = length(data.aws_availability_zones.available.names)
+  count                     = local.reverse_proxy_enabled[local.environment] ? length(data.aws_availability_zones.available.names) : 0
   route_table_id            = aws_route_table.reverse_proxy_private[count.index].id
   destination_cidr_block    = data.terraform_remote_state.ingest.outputs.ingestion_vpc.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.reverse_proxy_dev[0].id
+  vpc_peering_connection_id = aws_vpc_peering_connection.reverse_proxy[0].id
 }
 
 resource "aws_route" "ingest_to_reverse_proxy" {
   count                     = local.reverse_proxy_enabled[local.environment] ? 1 : 0
   route_table_id            = data.terraform_remote_state.ingest.outputs.emr_route_table.id
   destination_cidr_block    = module.vpc.vpc.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.reverse_proxy_dev[0].id
+  vpc_peering_connection_id = aws_vpc_peering_connection.reverse_proxy[0].id
   provider                  = aws.target
 }
 
 resource "aws_vpc_peering_connection_accepter" "reverse_proxy_ingest" {
   count                     = local.reverse_proxy_enabled[local.environment] ? 1 : 0
-  vpc_peering_connection_id = aws_vpc_peering_connection.reverse_proxy_dev[0].id
+  vpc_peering_connection_id = aws_vpc_peering_connection.reverse_proxy[0].id
   auto_accept               = true
 
   tags = merge(
