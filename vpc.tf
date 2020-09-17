ab0d3405eecb692d17965702c9d01554740e66fb
Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>
2020-09-17 10:19:58 +0100

    DW-4624 - Migrate to latest VPC module
    Signed-off-by: Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source                                   = "dwp/vpc/aws"
-  version                                  = "3.0.7"
+  version                                  = "3.0.8"
   vpc_name                                 = "internet-ingress"
   region                                   = var.region
   vpc_cidr_block                           = local.cidr_block[local.environment]["internet-ingress-vpc"]
